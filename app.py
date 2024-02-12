dbff916fee5aab9a179eabf49cd27110a5085454
Alfredo Castaneda <70920990+casalf724@users.noreply.github.com>
2024-02-12 17:14:14 +0000

    Half way working setup

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.get("/")
+@app.get("/") # Go to home page and render template
 def index_get():
     return render_template("base.html")
 
@@ -14,9 +14,9 @@ def index_get():
 def predict():
     text = request.get_json().get("message")
     # TODO: check if test is valid
-    response = get_response(text)
-    message = {"answer": response}
-    return jsonify(message)
+    response = get_response(text) # gets response from text
+    message = {"answer": response} # messages user
+    return jsonify(message) # message returned in json format
 
 
 if __name__ == "__main__":
