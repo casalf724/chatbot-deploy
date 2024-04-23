f1b5592573e27ede50526877e02073ae2bb56f81
Alfredo Castaneda <70920990+casalf724@users.noreply.github.com>
2024-04-23 19:30:24 +0000

    cleaned up app json file

--- standalone-frontend/app.js
+++ standalone-frontend/app.js
@@ -1,9 +1,9 @@
 class Chatbox {
     constructor() {
         this.args = {
-            openButton: document.querySelector(selectors: '.chatbox__button'),
-            chatBox: document.querySelector(selectors: '.chatbox__support'),
-            sendButton: document.querySelector(selectors:'.send__button')
+            openButton: document.querySelector('.chatbox__button'),
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button')
         }
 
         this.state = false;
@@ -11,76 +11,66 @@ class Chatbox {
     }
 
     display() {
-        const {openButton, chatBox, sendButton} = this.args;
+        const { openButton, chatBox, sendButton } = this.args;
 
-        openButton.addEventListener(type:'click', listener:() => this.toggleState(chatBox))
+        openButton.addEventListener('click', () => this.toggleState(chatBox));
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox));
 
-        sendButton.addEventListener(type:'click', listener:() => this.onSendButton(chatBox))
-
-        const node = chatBox.querySelector(selectors: 'input');
-        node.addEventListener(type: "keyup", listener:({key :string}) => {
-            if (key === "Enter") {
-                this.onSendButton(chatBox)
+        const node = chatBox.querySelector('input');
+        node.addEventListener("keyup", (event) => {
+            if (event.key === "Enter") {
+                this.onSendButton(chatBox);
             }
-        })
+        });
     }
 
     toggleState(chatbox) {
         this.state = !this.state;
-
-        // show or hides the box
-        if(this.state) {
-            chatbox.classList.add('chatbox--active')
+        if (this.state) {
+            chatbox.classList.add('chatbox--active');
         } else {
-            chatbox.classList.remove( tokens:'chatbox--active')
+            chatbox.classList.remove('chatbox--active');
         }
     }
 
     onSendButton(chatbox) {
         var textField = chatbox.querySelector('input');
-        let text1 = textField.value
-        if (text1 === "") {
+        let text = textField.value;
+        if (text === "") {
             return;
         }
 
-        let msg1 = { name: "User", message: text1 }
-        this.messages.push(msg1);
+        let msg = { name: "User", message: text };
+        this.messages.push(msg);
 
-        // 'http://127.0.0.1:5000/predict'
-        fetch(http://127.0.0.1:5000/predict' {
+        fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
-            body: JSON.stringify(value: { message: text1 }),
+            body: JSON.stringify({ message: text }),
             mode: 'cors',
             headers: {
-              'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             },
-          })
-          .then(r => r.json())
-          .then(r => {
-            let msg2 = { name: "Sam", message: r.answer };
+        })
+        .then(response => response.json())
+        .then(data => {
+            let msg2 = { name: "Sam", message: data.answer };
             this.messages.push(msg2);
-            this.updateChatText(chatbox)
-            textField.value = ''
-
-        }).catch((error) => {
+            this.updateChatText(chatbox);
+            textField.value = '';
+        })
+        .catch((error) => {
             console.error('Error:', error);
-            this.updateChatText(chatbox)
-            textField.value = ''
-          });
+            this.updateChatText(chatbox);
+            textField.value = '';
+        });
     }
 
     updateChatText(chatbox) {
         var html = '';
-        this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Hachiko")
-            {
-                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
-            }
-            else
-            {
-                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
-            }
-          });
+        this.messages.slice().reverse().forEach(function(item) {
+            let className = item.name === "Hachiko" ? 'messages__item--visitor' : 'messages__item--operator';
+            html += `<div class="messages__item ${className}">${item.message}</div>`;
+        });
 
         const chatmessage = chatbox.querySelector('.chatbox__messages');
         chatmessage.innerHTML = html;
