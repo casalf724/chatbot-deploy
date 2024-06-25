76648eebd1528e78caaf84667f78c8239010b8e4
Alfredo Castaneda <70920990+casalf724@users.noreply.github.com>
2024-06-25 23:03:04 +0000

    updated requirements and nltk download stuff

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, jsonify
 from chat import get_response  # Ensure this module is correctly implemented
 from flask_cors import CORS
 
+nltk.download('punkt')
+
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes, allowing requests from any domainquit
 
@@ -19,8 +21,8 @@ def get_chat_response():  # Function renamed for clarity and improved functional
     response = get_response(text)  # Generate response using the chat module
     return jsonify({"answer": response})  # Return the chat response in JSON format
 
-def download_nltk_resources():
-    nltk.download('punkt')  # Download the 'punkt' tokenizer models from NLTK
+# def download_nltk_resources():
+#   nltk.download('punkt')  # Download the 'punkt' tokenizer models from NLTK
 
 if __name__ == "__main__":
     download_nltk_resources()  # Ensure NLTK resources are downloaded before starting the app
